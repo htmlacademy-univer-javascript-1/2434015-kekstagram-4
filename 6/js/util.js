23cd3703b67c5f526123a40833b758657a7b2092
Keks <keks@htmlacademy.ru>
2023-12-01 09:31:07 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/util.js
@@ -0,0 +1,24 @@
+const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+// Вспомогательная функция #2 - поиск рандомного элемента массива
+
+const getRandomArrayElement = (array) =>
+  array[getRandomInteger(0, array.length - 1)];
+
+// Вспомогательная функция #3 - генерация ID
+
+function createIdGenerator () {
+  let lastGeneratedId = 0;
+
+  return () => {
+    lastGeneratedId += 1;
+    return lastGeneratedId;
+  };
+}
+
+export {getRandomInteger, getRandomArrayElement, createIdGenerator};
