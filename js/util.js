ebc7ceb59dfabaa8018ccc07ee16e9b3a7601016
Anne <128488854+Annette04@users.noreply.github.com>
2023-12-20 19:02:24 +0500

    master

--- js/util.js
+++ js/util.js
@@ -1,4 +1,5 @@
 const DEBOUNCE_INTERVAL = 500;
+const FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
 
 const Keys = {
   ESC: 'Esc',
@@ -44,11 +45,11 @@ const shuffle = (arr) => {
   return arr;
 };
 
-const checkingMaxLength = (text, count) => {
-  return text.length <= count;
+// words = [1, 2, 5] => [один символ, два символа, пять символов]
+const getWordEnding = (number, words) => {
+  const cases = [2, 0, 1, 1, 1, 2];
+  return words[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]];
 };
-
-// checkingMaxLength('Какая интересная фотка', POSTS_DATA.comment_max_length);
 
 const isEscEvent = (evt) => {
   return evt.key === Keys.ESC || evt.key === Keys.ESCAPE;
@@ -68,4 +69,16 @@ const debounce = (callback) => {
   };
 };
 
-export { getRandomInt, getRandomElement, getUniqueValue, removeDuplicate, shuffle, checkingMaxLength, isEscEvent, debounce };
+const getPhotoSrc = (fileChooser, onSuccess) => {
+  const file = fileChooser.files[0];
+  const fileName = file.name.toLowerCase();
+
+  if (FILE_TYPES.some((it) => fileName.endsWith(it))) {
+    const reader = new FileReader();
+
+    reader.readAsDataURL(file);
+    reader.addEventListener('load', () => onSuccess(reader.result), { once: true });
+  }
+};
+
+export { getRandomInt, getRandomElement, getUniqueValue, removeDuplicate, shuffle, getWordEnding, isEscEvent, debounce, getPhotoSrc };
